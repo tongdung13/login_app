367ade4a8f5f3f2548bf8668f89bb0b85914087a
tongdung13 <tongvanthienvu@gmail.com>
2023-01-12 21:03:58 +0700

    update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,9 +40,10 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:api',
+            'throttle:60,1',
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Session\Middleware\AuthenticateSession::class,
         ],
     ];
 
