11decf04616e7ee8d767dc9311e5571cf792c2a7
tongdung13 <tongvanthienvu@gmail.com>
2023-01-12 21:33:41 +0700

    up

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,6 @@ Route::post('login', [AuthController::class, 'login']);
 
 Route::group(['middleware' => ['guest']], function () {
     Route::prefix('blogs')->group(function () {
-        Route::get('', [BlogController::class, 'index']);
+        Route::get('', [BlogController::class, 'index'])->withoutMiddleware("throttle:api");
     });
 });
